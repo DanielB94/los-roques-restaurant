b08ab8e36614289aab625fdf368d05b4177f4074
Daniel Bermudez <josedbr18.respaldo@gmail.com>
2024-12-04 12:55:31 -0600

    adding porduction route

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3200';
+const URL = process.env.NODE_ENV === 'production' ? 'https://www.losroquesrestaurant.com' : 'http://localhost:3200';
 
 export const socket = io(URL, { autoConnect: false });
