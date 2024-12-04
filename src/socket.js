2cb04cf251359ba4712d47f8cbf7b991e580911d
Daniel Bermudez <josedbr18.respaldo@gmail.com>
2024-12-04 13:32:11 -0600

    adding useEffect to adminLog to connect to socket

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://www.losroquesrestaurant.com';
+const URL = 'https://api.losroquesrestaurant.com';
 
 export const socket = io(URL, { autoConnect: false });
