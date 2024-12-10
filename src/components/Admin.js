fac27fbb0c2aec87994b807b715a593dedb2adca
Daniel Bermudez <josedbr18.respaldo@gmail.com>
2024-12-09 22:36:11 -0600

    adding a button with its function and a status indicator

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -84,7 +84,10 @@ const Admin = (props) => {
   /// FUNCTION TO GET ORDERS IN CASE SOMETHING GOES WRONG ///
   const backupHandler = () => {
     axios.get(`${apiUrl}/adminApi/get-order`, { headers: { Authorization: `${localStorage.getItem('token')}`}})
-    .then(result => console.log(result))
+    .then(result => {
+      if (result.data.length !== 0) {
+      setOrderFromIo(result.data);
+    }})
     .catch(err => console.log(err))
   }
 
