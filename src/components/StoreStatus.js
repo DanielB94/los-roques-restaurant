8323e1b8c84b5eb4ca7f74af1bb0ae368526b566
Daniel Bermudez <josedbr18.respaldo@gmail.com>
2024-12-24 10:53:29 -0600

    adding open/close logic

--- src/components/StoreStatus.js
+++ src/components/StoreStatus.js
@@ -3,7 +3,7 @@ import React from 'react'
 const StoreStatus = () => {
   return (
     <div>
-      <h1>Hola, el horario de atencion es de 6:00pm a 12:00am</h1>
+      <h1>El horario para hacer pedidos es de 6:00pm a 12:00am</h1> <p>Por consiguiente no podras agregar platos al carrito de compras, te invitamos a ver nuestro menu</p>
     </div>
   )
 }
