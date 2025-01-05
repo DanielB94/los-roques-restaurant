635b326dcab8d37afaf1ba087e681b88e9d9782c
Daniel Bermudez <josedbr18.respaldo@gmail.com>
2025-01-04 22:39:26 -0600

    finishing delivery price logic

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -3,14 +3,13 @@ import React, { useContext, useRef, useState } from 'react';
 import { ApiUrlContext } from '../context/ApiUrlContext';
 import { Autocomplete, GoogleMap, useJsApiLoader, useLoadScript, StandaloneSearchBox } from '@react-google-maps/api';
 
-
 const Delivery = (props) => {
 
     const libraries = ['places'];
 
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
-        googleMapsApiKey: 'AIzaSyBy0XJopJw9Ls3XjTlBaHu_ZBfZV5z3Jvs', // Replace with your actual API key
+        googleMapsApiKey: process.env.GOOGLE_API, // Replace with your actual API key
         libraries: ['places']
       });
 
@@ -27,17 +26,23 @@ const Delivery = (props) => {
 
     const deliveryHandler = async (e) => {
         e.preventDefault();
-        setDestination(`${direction} ${houseNumber} ${zipCode}`);
         
         const result = await axios.post(`${apiUrl}/api/delivery`, {destination}, { withCredentials: true })
+        
         let value = result.data.distance[0].map(r => r.value);
         setValueTotal((value / 1000) / 1.6 * 1.7);
+        setDirection(`${destination} ${houseNumber}`);
+        console.log(value);
         console.log(valueTotal);
+        console.log(direction);
     }
-
+    
     const handleOnPlaceChanged = () => {
         let address = autocompleteRef.current.getPlaces();
         console.log(address);
+        setDirection(address[0].formatted_address);
+        setDestination(address[0].formatted_address);
+        console.log(direction);
     }
 
   return (
@@ -48,12 +53,13 @@ const Delivery = (props) => {
                 onLoad={(ref) => autocompleteRef.current = ref}
                 onPlacesChanged={handleOnPlaceChanged}>
 
-                <input type="text" placeholder='Direccion' onChange={(e) => {setDirection(e.target.value); console.log(direction)}} required/>
+                <input type="text" placeholder='Direccion' required/>
             </StandaloneSearchBox>
         <input type="text" placeholder='Numero casa/apto' onChange={(e) => setHouseNumber(e.target.value)} required/>
         <button>Consultar</button>
       </form>
         }
+        {!valueTotal ? null : valueTotal}
     </div>
   )
 }
