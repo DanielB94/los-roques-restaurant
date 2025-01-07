71e0aea0f68e39a00c45ce6c589bddd67a0803c2
Daniel Bermudez <josedbr18.respaldo@gmail.com>
2025-01-06 23:37:32 -0600

    finishing Delivery module

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useContext, useRef, useState } from 'react';
 import { ApiUrlContext } from '../context/ApiUrlContext';
 import { Autocomplete, GoogleMap, useJsApiLoader, useLoadScript, StandaloneSearchBox } from '@react-google-maps/api';
+import './styles/deliveryModal.css'
 
 const Delivery = (props) => {
 
@@ -9,11 +10,11 @@ const Delivery = (props) => {
 
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
-        googleMapsApiKey: process.env.GOOGLE_API, // Replace with your actual API key
+        googleMapsApiKey: 'AIzaSyD0nch8V776CVz-Ys6tmNdEOS-1wQM9Dzw', // Replace with your actual API key
         libraries: ['places']
       });
 
-    const { destination, setDestination } = props;
+    const { destination, setDestination, deliveryTotal, setDeliveryTotal, deliveryModal, setDeliveryModal, handlerAddButton } = props;
 
     const apiUrl = useContext(ApiUrlContext);
 
@@ -21,19 +22,25 @@ const Delivery = (props) => {
 
     const [direction, setDirection] = useState(null);
     const [houseNumber, setHouseNumber] = useState(null);
-    const [zipCode, setZipCode] = useState(null);
-    const [ valueTotal, setValueTotal] = useState(null);
 
-    const deliveryHandler = async (e) => {
+    const deliveryPriceHandler = async (e) => {
         e.preventDefault();
         
         const result = await axios.post(`${apiUrl}/api/delivery`, {destination}, { withCredentials: true })
         
         let value = result.data.distance[0].map(r => r.value);
-        setValueTotal((value / 1000) / 1.6 * 1.7);
+        let valueInMiles = value / 1000 / 1.6;
+        let price = valueInMiles * 1.7;
+        let formattedPrice = price.toFixed(2);
+
+        if (valueInMiles <= 5 ) {
+            setDeliveryTotal(8.5);
+        } else {
+            setDeliveryTotal(formattedPrice);
+        }
         setDirection(`${destination} ${houseNumber}`);
         console.log(value);
-        console.log(valueTotal);
+        console.log(deliveryTotal);
         console.log(direction);
     }
     
@@ -45,21 +52,50 @@ const Delivery = (props) => {
         console.log(direction);
     }
 
-  return (
-    <div>
-        {!isLoaded ? <div>Loading...</div> : 
-            <form onSubmit={deliveryHandler}>
-            <StandaloneSearchBox
-                onLoad={(ref) => autocompleteRef.current = ref}
-                onPlacesChanged={handleOnPlaceChanged}>
+    const cancelDelivery = () => {
+        setDeliveryTotal(0);
+        setDeliveryModal(false);
+    }
 
-                <input type="text" placeholder='Direccion' required/>
-            </StandaloneSearchBox>
-        <input type="text" placeholder='Numero casa/apto' onChange={(e) => setHouseNumber(e.target.value)} required/>
-        <button>Consultar</button>
-      </form>
+    const product = {
+        name: 'Delivery',
+        price: deliveryTotal,
+        priceInCents: deliveryModal * 100,
+        image: undefined,
+        category: undefined,
+        reward: undefined,
+        quantity: 1
+    }
+
+    const deliveryHandler = (product) => {
+        handlerAddButton(product)
+        setDeliveryModal(false);
+    }
+
+  return (
+    <div className='deliveryContainer'>
+        {!isLoaded ? <div>Loading...</div> :
+            <div className='deliveryModal'>
+                <button className='x' onClick={cancelDelivery}>X</button>
+                <form onSubmit={deliveryPriceHandler}>
+                    <StandaloneSearchBox
+                        onLoad={(ref) => autocompleteRef.current = ref}
+                        onPlacesChanged={handleOnPlaceChanged}>
+
+                        <input type="text" placeholder='Direccion' required/>
+                    </StandaloneSearchBox>
+                    <input type="text" placeholder='Numero casa/apto' onChange={(e) => setHouseNumber(e.target.value)} required/>
+                    <button className='cta'>Consultar</button>
+                </form>
+                {deliveryTotal === 0 ? null :
+                    <div className='deliveryInfo'>
+                        <p>El precio del delivery es de: ${deliveryTotal}</p>
+                        <button className='cta' onClick={deliveryHandler(product)}>Agregar delivery</button>
+                        <button className='cta' onClick={cancelDelivery}>Cancelar</button>
+                    </div>
+                }
+            </div>
         }
-        {!valueTotal ? null : valueTotal}
     </div>
   )
 }
